efa23515e97a1ffc46b44d3f5115ea505e91eaaf
Shivam Chauhan <142312168+ShivamChauhan-15@users.noreply.github.com>
2024-06-08 17:25:59 +0530

    first commit

---
+++ ExtractDetailsFromTest/src/main/java/com/example/ExtractDetailsFromTest/ExtractDetailsFromTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExtractDetailsFromTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractDetailsFromTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractDetailsFromTestApplication.class, args);
+	}
+
+}
---
+++ ExtractDetailsFromTest/src/test/java/com/example/ExtractDetailsFromTest/ExtractDetailsFromTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExtractDetailsFromTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtractDetailsFromTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
